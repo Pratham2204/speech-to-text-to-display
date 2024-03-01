69f09d4010eeabcb4ef3d137457912afbc72d8c6
Pratham2204 <96829659+Pratham2204@users.noreply.github.com>
2024-03-02 01:24:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import speech_recognition as sr
+import keyboard
+import pyaudio
+
+def listen_and_recognize():
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    with microphone as source:
+        print("Please hold the spacebar to talk...")
+        recognizer.adjust_for_ambient_noise(source)  # Adjust for ambient noise once
+
+        # Start recording if the spacebar is pressed
+        keyboard.wait('space')
+        print("Listening...")
+        audio = recognizer.listen(source, phrase_time_limit=10)  # Adjust time limit for your needs
+
+    # Recognize speech using Google Web Speech API 
+    try:
+        print("Processing speech...")
+        text = recognizer.recognize_google(audio)
+        print("You said: " + text)
+        with open(r'C:\Users\Pratham jain\Desktop\RIT\rit\semister 8\major project\speech-to-text\op.txt', 'a') as f:
+            f.write(text + '\n')
+        f.close()
+    except sr.UnknownValueError:
+        print("Google Web Speech API could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Web Speech API; {0}".format(e))
+
+if __name__ == "__main__":
+    while True:
+        listen_and_recognize()
